bee789e7272c185d5117e6f4040ac4a8331027dd
Gigacee <gigacee@gigacreation.jp>
2023-07-21 01:40:41 +0900

    Add project

---
+++ Assets/CsvParser/Runtime/CsvParser.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GigaCreation.Tools.CsvParser
+{
+    public static class CsvParser
+    {
+        public static List<List<string>> Parse(string csv)
+        {
+            var reader = new StringReader(csv);
+            var result = new List<List<string>>();
+
+            while (reader.Peek() > -1)
+            {
+                string line = reader.ReadLine();
+
+                // `line` is null if the end of the string is reached.
+                if (line == null)
+                {
+                    break;
+                }
+
+                result.Add(SplitLine(line));
+            }
+
+            return result;
+        }
+
+        public static List<Dictionary<string, string>> ParseIntoDictionaries(string csv)
+        {
+            List<List<string>> table = Parse(csv);
+
+            if ((table.Count == 0) || table[0].Any(string.IsNullOrEmpty))
+            {
+                Debug.LogWarning("Some or all of the headers are empty.");
+                return null;
+            }
+
+            if (table[0].GroupBy(header => header).Any(grouping => grouping.Count() > 1))
+            {
+                Debug.LogWarning("Duplicate header found.");
+                return null;
+            }
+
+            var result = new List<Dictionary<string, string>>(table.Count - 1);
+
+            for (var rowIndex = 1; rowIndex < table.Count; rowIndex++)
+            {
+                var dict = new Dictionary<string, string>(table[0].Count);
+
+                for (var columnIndex = 0; columnIndex < table[0].Count; columnIndex++)
+                {
+                    dict.Add(table[0][columnIndex], table[rowIndex][columnIndex]);
+                }
+
+                result.Add(dict);
+            }
+
+            return result;
+        }
+
+        private static List<string> SplitLine(string line)
+        {
+            var columns = new List<string>();
+            var builder = new StringBuilder(line.Length);
+            var isInLiteral = false;
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                if ((line[i] == ',') && !isInLiteral)
+                {
+                    columns.Add(builder.ToString());
+                    builder.Clear();
+
+                    continue;
+                }
+
+                if (line[i] == '\"')
+                {
+                    if ((i < line.Length - 1) && (line[i + 1] == '\"'))
+                    {
+                        builder.Append('\"');
+                        i++;
+                    }
+                    else
+                    {
+                        isInLiteral = !isInLiteral;
+                    }
+
+                    continue;
+                }
+
+                builder.Append(line[i]);
+            }
+
+            columns.Add(builder.ToString());
+
+            return columns;
+        }
+    }
+}
---
+++ Assets/CsvParser/Sample~/Scripts/CsvParserSample.cs
@@ -0,0 +1,60 @@
+// ReSharper disable ArrangeObjectCreationWhenTypeEvident
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace GigaCreation.Tools.CsvParser.Sample
+{
+    public class CsvParserSample : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform _table;
+        [SerializeField] private Transform _column;
+        [SerializeField] private Transform _cell;
+
+        [Header("Parameters")]
+        [SerializeField] private string _csvPath;
+
+        private readonly StringBuilder _keysBuilder = new StringBuilder();
+        private readonly StringBuilder _valuesBuilder = new();
+        private readonly StringBuilder _equalityOperatorBuilder = new();
+
+        private void Start()
+        {
+            string csv = Resources.Load<TextAsset>(_csvPath).text;
+
+            List<List<string>> table = CsvParser.Parse(csv);
+
+            int numOfRows = table.Count;
+            int numOfColumns = table.Max(row => row.Count);
+
+            for (var i = 0; i < numOfRows - 1; i++)
+            {
+                Instantiate(_cell, _cell.parent);
+            }
+
+            for (var j = 0; j < numOfColumns - 1; j++)
+            {
+                Instantiate(_column, _column.parent);
+            }
+
+            Transform[] columns = _table.Cast<Transform>().ToArray();
+
+            for (var columnIndex = 0; columnIndex < columns.Length; columnIndex++)
+            {
+                Transform[] cells = columns[columnIndex].Cast<Transform>().ToArray();
+
+                for (var rowIndex = 0; rowIndex < cells.Length; rowIndex++)
+                {
+                    string text = table[rowIndex].Count > columnIndex ? table[rowIndex][columnIndex] : "";
+                    cells[rowIndex].GetComponent<TextMeshProUGUI>().SetText(text);
+                }
+            }
+
+            Debug.Log(table);
+        }
+    }
+}
